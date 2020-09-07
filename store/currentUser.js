d8f536ac65fefc14ad9687f9f1cf623e23f07de2
James C. Tucker <jamesctucker@protonmail.com>
2020-09-07 00:06:13 -0500

    added currentUser store for future auth with aws amplify

---
+++ store/currentUser.js
@@ -0,0 +1,11 @@
+const state = () => ({
+  id: 1,
+  name: "James Tucker",
+  email: "jamesctucker@protonmail.com"
+});
+
+const mutations = () => {};
+
+const actions = () => {};
+
+export { state, mutations, actions };
